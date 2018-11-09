1d93fc478b13395130bbe17311f744c01cb470d6
nignanthomas <nignanthomas@gmail.com>
2018-11-09 19:46:43 +0300

    create class Source in models/source.py

---
+++ app/models/source.py
@@ -0,0 +1,12 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        
