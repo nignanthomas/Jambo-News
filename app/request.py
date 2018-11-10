6564582154e5d703d69a941d2700829fda69ea17
nignanthomas <nignanthomas@gmail.com>
2018-11-10 05:11:54 +0300

    create get_source function to request sources by category

--- app/request.py
+++ app/request.py
@@ -9,3 +9,28 @@ api_key = app.config['NEWS_API_KEY']
 
 #Getting the news base url
 base_url = app.config['SOURCE_API_BASE_URL']
+
+
+
+
+def get_sources(category):
+    '''
+    Function that get the json response to our url request.
+    '''
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+    return source_results
+
+
+
+    
