ca382b1fa26542ff7128bdd5538354e73143511d
nignanthomas <nignanthomas@gmail.com>
2018-11-10 15:24:39 +0300

    create Articles class

---
+++ app/models/articles.py
@@ -0,0 +1,11 @@
+class Articles:
+    '''
+    Articles class to define Articles Objects
+    '''
+
+    def __init__(self,title,image,description,url,date):
+        self.title = title
+        self.image = image
+        self.description = description
+        self.url = url
+        self.date = date
