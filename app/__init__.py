c5029c2c986c705945009c2645b23b00788ab550
nignanthomas <nignanthomas@gmail.com>
2018-11-11 10:12:08 +0300

    try custom css - not working

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,19 @@ from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
 
+
+
 #Initializing
 app = Flask(__name__,instance_relative_config = True)
 
 #Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+
+app.static_folder = 'static'
+
+
 
 #Initializing Flask Extensions
 bootstrap = Bootstrap(app)
